4a0b9106066d4f26eff60d307eeda26f7cc176cc
Sohrat <sohrat6128@gmail.com>
2021-02-16 17:46:27 +0300

    create user model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim:true
+    },
+    job: {
+        type: String,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowerCase: true,
+        trim:true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Not a valid email");
+            };
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+
+module.exports = User;
