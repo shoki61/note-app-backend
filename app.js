bb3a133d47ce2f0da3388e9a3abcfd2a4dfeb22e
Sohrat <sohrat6128@gmail.com>
2021-02-18 19:15:07 +0300

    set sign up by connecting from frontend to beckend

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ const app = express();
 
 
 app.use(bodyParser.json());
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH');
+    next();
+});
 app.use('/api/users', usersRouter);
 app.use('/api/notes', notesRouter);
 
 
 
-mongoose.connect('mongodb+srv://shoki:murtishoki61@cluster0.sbiw4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://shoki:murtishoki61@cluster0.sbiw4.mongodb.net/mern?retryWrites=true&w=majority')
     .then(() => app.listen(5000))
     .catch(error => console.log(error));
---
+++ test.js
@@ -0,0 +1,10 @@
+
+
+const x = 5;
+const y = 3;
+
+if(false || true){
+    console.log(true);
+}else{
+    console.log(false);
+}
