5aff737b1a221195ed768e09a7a954584ba44b55
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-04-03 13:24:27 +0300

    set nodemon and env variable

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.use((error, req, res, next) => {
 });
 
 
-mongoose.connect('mongodb+srv://shoki:murtishoki61@cluster0.sbiw4.mongodb.net/mern?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sbiw4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
     .then(() => app.listen(5000))
     .catch(error => console.log(error));
